1df62bd30659e43501d1f31cdbc5a08206ffadb6
Eng Angelo Saber <74116325+AngeloXcode@users.noreply.github.com>
2021-10-27 14:23:21 +0300

    add Array of Movies

--- BuildNetflix/BuildNetflix/View/HomeView.swift
+++ BuildNetflix/BuildNetflix/View/HomeView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct HomeView: View {
+    var viewModel = HomeViewModel()
     var body: some View {
         ZStack{
             Color.black
                 .edgesIgnoringSafeArea(.all)
-        }
+            
+            ForEach(viewModel.allCategories,id:\.self){ catgory in
+                VStack {
+                    HStack {
+                        Text(catgory)
+                        Spacer()
+                    }
+                    ScrollView(.horizontal,showsIndicators: false){
+                        HStack{
+                            ForEach(viewModel.getMovie(catgory)){ movie in
+                                StandardHomeMovieView(movie: movie)
+                                    .frame(width:100,height:200)
+                                    .padding(.horizontal,20)
+                            }
+                        }
+
+                    }
+                }
+                    
+                    
+            }
+        }.foregroundColor(.white)
     }
 }
 
---
+++ Netflix Clone/Netflix_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_CloneApp.swift
+//  Netflix Clone
+//
+//  Created by Angelo Essam on 27/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
