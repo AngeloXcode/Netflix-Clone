663529623e6f7379752e12138ef478a4592337f4
Eng Angelo Saber <74116325+AngeloXcode@users.noreply.github.com>
2021-10-28 08:57:12 +0300

    Top Menu
    Add Top Menu

--- BuildNetflix/BuildNetflix/View/HomeView.swift
+++ BuildNetflix/BuildNetflix/View/HomeView.swift
@@ -18,33 +18,41 @@ struct HomeView: View {
                 .edgesIgnoringSafeArea(.all)
             ScrollView(showsIndicators: true){
                 LazyVStack{
+                    
+                    
+                    TopMenuButtonsView()
+                    
+                    
                     TopMovieView(movie: mockData)
                         .frame(width:screen.width)
                         .padding(.top,-100)
-                        
-                ForEach(viewModel.allCategories,id:\.self){ catgory in
-                    VStack {
-                        HStack {
-                            Text(catgory)
-                                .font(.title3)
-                                .bold()
-                            Spacer()
-                        }
-                        ScrollView(.horizontal,showsIndicators: false){
-                            LazyHStack{
-                                ForEach(viewModel.getMovieWhereCategory(catgory)){ movie in
-                                    StandardHomeMovieView(movie: movie)
-                                        .frame(width:100,height:200)
-                                        .padding(.horizontal,20)
-                                }
+                        .zIndex(-1)
+                    
+                    
+                    
+                    ForEach(viewModel.allCategories,id:\.self){ catgory in
+                        VStack {
+                            HStack {
+                                Text(catgory)
+                                    .font(.title3)
+                                    .bold()
+                                Spacer()
                             }
-
+                            ScrollView(.horizontal,showsIndicators: false){
+                                LazyHStack{
+                                    ForEach(viewModel.getMovieWhereCategory(catgory)){ movie in
+                                        StandardHomeMovieView(movie: movie)
+                                            .frame(width:100,height:200)
+                                            .padding(.horizontal,20)
+                                    }
+                                }
+                                
+                            }
                         }
-                    }
                         
                         
-                } // end of for loop
-            }
+                    } // end of for loop
+                }
             } //end of ScrollView
         }.foregroundColor(.white)
     }
@@ -53,5 +61,65 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+    }
+}
+
+struct TopMenuButtonsView : View {
+    var body: some View {
+        HStack {
+            Button(action: {
+                // action here
+                
+            }, label: {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50.0)
+            }).buttonStyle(PlainButtonStyle())
+            
+            
+            Spacer()
+            
+            Button(action: {
+                // action here
+                
+            }, label: {
+                Text("TV Shows")
+            }).buttonStyle(PlainButtonStyle())
+            
+            
+            Spacer()
+            
+            
+            Button(action: {
+                // action here
+                
+            }, label: {
+                Text("Movies")
+            }).buttonStyle(PlainButtonStyle())
+            
+            Spacer()
+            
+            Button(action: {
+                // action here
+                
+            }, label: {
+                Text("Series")
+            }).buttonStyle(PlainButtonStyle())
+            
+            Spacer()
+            
+            Button(action: {
+                // action here
+                
+            }, label: {
+                Text("My List")
+            }).buttonStyle(PlainButtonStyle())
+            
+            
+        }
+        .background(Color.black.opacity(0.1))
+        .padding(.trailing,30)
+        .padding(.leading,10)
     }
 }
